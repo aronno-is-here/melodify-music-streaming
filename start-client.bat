9cdc8109d94002ab620c443408c1b171f953a08b
aronno-is-here <aronno2000@gmail.com>
2026-09-15 02:13:08 +0600

    chore: remove legacy start scripts, update .gitignore

--- start-client.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "D:\Aronno\Works\Melodify - Music Streaming Website\client"
-npx vite --host
--- start-server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "D:\Aronno\Works\Melodify - Music Streaming Website\server"
-node server.js
